d07cd6a68c295787850fcfb289a7fe28c0af67d6
millionaiyer <surajsaiyer@gmail.com>
2025-07-16 07:43:12 +0000

    tailwind setup with turbo  repo

--- apps/partner/src/routes/+page.svelte
+++ apps/partner/src/routes/+page.svelte
@@ -3,9 +3,9 @@
 	import { MyCounterButton } from '../../../../packages/ui';
 </script>
 
-<div class="flex w-full h-full">
+	<h1>Welcome to SvelteKit</h1>
+	<p class="">
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+	<MyCounterButton />
 	<Header />
-</div>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<MyCounterButton />
